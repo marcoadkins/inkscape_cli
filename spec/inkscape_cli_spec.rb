0c7e889732feb8cf30264b8a33f6f812f8197c4d
Marco Adkins <10146618+marcoadkins@users.noreply.github.com>
2023-03-31 14:30:28 -0400

    Drop failing test

--- spec/inkscape_cli_spec.rb
+++ spec/inkscape_cli_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe InkscapeCli do
   it 'has a version number' do
     expect(InkscapeCli::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
